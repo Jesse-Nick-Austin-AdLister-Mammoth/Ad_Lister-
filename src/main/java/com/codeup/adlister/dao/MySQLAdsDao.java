d57127a96ffc7da421975ad2d55638a37daa99cf
SHFFLE <mistersh1ffle@gmail.com>
2017-01-03 12:29:49 -0600

    Added ability to create an ad with unspecified category

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -111,17 +111,30 @@ public class MySQLAdsDao implements Ads {
     @Override
     public Long insert(Ad ad) {
         try {
-            String insertQuery = "INSERT INTO ads(user_id, title, description, imgpath, category) VALUES (?, ?, ?, ?, ?)";
-            PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
-            stmt.setLong(1, ad.getUserId());
-            stmt.setString(2, ad.getTitle());
-            stmt.setString(3, ad.getDescription());
-            stmt.setString(4, ad.getUrl());
-            stmt.setInt(5, ad.getCategoryInt());
-            stmt.executeUpdate();
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next();
-            return rs.getLong(1);
+            if(ad.getCategoryInt() > 0){
+                String insertQuery = "INSERT INTO ads(user_id, title, description, imgpath, category) VALUES (?, ?, ?, ?, ?)";
+                PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+                stmt.setLong(1, ad.getUserId());
+                stmt.setString(2, ad.getTitle());
+                stmt.setString(3, ad.getDescription());
+                stmt.setString(4, ad.getUrl());
+                stmt.setInt(5, ad.getCategoryInt());
+                stmt.executeUpdate();
+                ResultSet rs = stmt.getGeneratedKeys();
+                rs.next();
+                return rs.getLong(1);
+            } else {
+                String insertQuery = "INSERT INTO ads(user_id, title, description, imgpath) VALUES (?, ?, ?, ?)";
+                PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+                stmt.setLong(1, ad.getUserId());
+                stmt.setString(2, ad.getTitle());
+                stmt.setString(3, ad.getDescription());
+                stmt.setString(4, ad.getUrl());
+                stmt.executeUpdate();
+                ResultSet rs = stmt.getGeneratedKeys();
+                rs.next();
+                return rs.getLong(1);
+            }
         } catch (SQLException e) {
             throw new RuntimeException("Error creating a new ad.", e);
         }
